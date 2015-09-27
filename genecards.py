832d4751b362a42914684eb867f66d9ffaa33c51
Perry <samesense@gmail.com>
2015-09-27 11:35:57 -0400

    Rm old code.

--- genecards.py
+++ genecards.py
@@ -1,4 +1,3 @@
-import scrapy
 import requests, bs4
 
 baseUrl = 'http://www.genecards.org/'
@@ -22,24 +21,3 @@ def getEnsemblGene(gene):
                         ensemblGene = str(link2).split('gene=')[1].split('"')[0]
                         ensemblGeneSet.add(ensemblGene)
     return ensemblGeneSet
-
-def testBraf():
-    gene = 'BRAF'
-    searchUrl = baseUrl + 'Search/Keyword?queryString=%s' % (gene, )
-    response = requests.get(searchUrl)
-    soup = bs4.BeautifulSoup(response.text)
-    links = soup.find_all("td", class_="gc-gene-symbol")
-    ensemblGenes = set()
-    for link in links:
-        for item in link:
-            name = str(item).split('data-ga-label="')[1].split('"')[0]
-            href = baseUrl + str(item).split('href="')[1].split('"')[0].split('&')[0]
-            if name == gene:
-                response2 = requests.get(href)
-                soup2 = bs4.BeautifulSoup(response2.text)
-                links2 = soup2.find_all("a", class_="gc-ga-link")
-                for link2 in links2:
-                    if 'ensembl' in str(link2) and 'geneview' in str(link2):
-                        ensemblGene = str(link2).split('gene=')[1].split('"')[0]
-                        ensemblGenes.add(ensemblGene)
-    return ensemblGenesls
